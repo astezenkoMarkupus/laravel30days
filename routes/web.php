d542e62a491b982639d287a3ef064104fb936ae3
Andrei Stezenko <astezenko@markupus.tech>
2024-08-18 17:29:31 +0300

    feat: add Job model

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,16 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', fn () => view('home'));
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$100,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$35,000'
-            ]
-        ]
-    ]);
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$100,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$35,000'
-        ]
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
-    return view('job', ['job' => $job]);
+    return view('job', ['job' => Job::find($id)]);
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', fn () => view('contact'));
