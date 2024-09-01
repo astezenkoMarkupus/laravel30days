496d105d6a6e173e78a796c4f30ebcaab300a036
Andrei Stezenko <astezenko@markupus.tech>
2024-09-02 00:08:09 +0300

    feat: add middleware for routes

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -1,4 +1,3 @@
-@php use Illuminate\Support\Facades\Auth; @endphp
 <x-layout>
   <x-slot:title>
     Job: {{ $job->title }}
@@ -8,10 +7,10 @@
     <strong>Salary:</strong> {{ $job->salary }} per year
   </div>
 
-  @if($isAuthor)
+  @can('edit-job', $job)
     <br>
     <x-link class="text-blue-500 hover:underline" href="/jobs/{{ $job->id }}/edit">Edit</x-link>
-  @endif
+  @endcan
 
   <br> <br>
   <x-link class="text-blue-500 hover:underline" href="/jobs">Back to Jobs List</x-link>
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,21 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-Route::resource('jobs', JobController::class);
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/{job}', [JobController::class, 'show']);
+Route::get('/jobs/create', [JobController::class, 'create'])
+    ->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])
+    ->middleware('auth');
+Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
+    ->middleware('auth')
+    ->can('edit-job', 'job');
+Route::patch('/jobs/{job}', [JobController::class, 'update'])
+    ->middleware('auth')
+    ->can('edit-job', 'job');
+Route::delete('/jobs/{job}', [JobController::class, 'destroy'])
+    ->middleware('auth')
+    ->can('delete-job', 'job');
 
 // Auth
 Route::get('/register', [RegisteredUserController::class, 'create']);
